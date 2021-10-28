d04756b9cbc4f31c4ff63ecba9d7f414ed5f13fd
cdrummo <32054606+cdrummo@users.noreply.github.com>
2021-10-27 19:59:30 -0600

    Added features:   1. Footer   2. Room editing and deleting   3. Validation for forms   4. Easter Egg! :)
    Updated:
      1. Navbar Brand
      2. Navbar User buttons
      3. Overall CSS and Layout of pages in the App.
      4. User editing page, still some features unavailable :(

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
   has_many :room_messages, dependent: :destroy
 
-  validates :name, length: { minimum: 2 }
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :username, uniqueness: true, presence: true
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
+
+  validates :username,
+            uniqueness: true,
+            presence: true,
+            length: { minimum: 2, maximum: 16 },
+            format: { with: /\A[a-zA-Z0-9]+\z/,
+                      message: "only allows letters and numbers, no special characters" }
 end
